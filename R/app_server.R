5ba1674a03814648ea98a7b55fe4ffd621ce3c51
DivadNojnarg <dgranjon@ymail.com>
2022-04-28 16:38:33 +0200

    add lag

--- R/app_server.R
+++ R/app_server.R
@@ -7,6 +7,7 @@
 app_server <- function(input, output, session) {
   # Your application server logic
   output$data <- renderTable({
+    Sys.sleep(4)
     mtcars[, c("mpg", input$variable), drop = FALSE]
   }, rownames = TRUE)
 }
